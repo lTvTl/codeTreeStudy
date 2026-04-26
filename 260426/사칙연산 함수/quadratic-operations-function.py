18c8e248047bcc61b51fd6c196d7b1d1eb59665c
Park Jinsung <87609868+lTvTl@users.noreply.github.com>
2026-04-26 21:30:35 +0900

    Title: 사칙연산 함수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260426/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,30 @@
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+# Please write your code here.
+
+def dohagi(a, c):
+    return print(a,'+', c,'=', a + c)
+
+def pegi(a, c):
+    return print(a,'-', c,'=', a - c)
+
+def nanugi(a, c):
+    return print(a,'/', c,'=', a / c)
+
+def goup(a, c):
+    return print(a,'*', c,'=', a * c)
+
+
+if o == '+':
+    dohagi(a, c)
+elif o == '-':
+    pegi(a, c)
+elif o == '/':
+    nanugi(a, c)
+elif o == '*':
+    goup(a, c)
+else:
+    print('False')
+
